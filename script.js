64ea98087b996c1545a0cabb051020b7642d36b8
NickFranca-crypto <nnick7142@gmail.com>
2024-09-24 20:06:37 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const attractions = document.querySelectorAll('.attraction');
-
-    attractions.forEach(attraction => {
-        attraction.addEventListener('click', () => {
-            alert(`Você clicou em: ${attraction.querySelector('h3').innerText}`);
-        });
-    });
-});
